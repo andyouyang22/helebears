178ac5d656f9e3fa8cc3d9269695862a47a4aeaf
ryankashi <ryankashi@berkeley.edu>
2015-10-16 20:40:23 -0700

    Populate Departments from inside Javascript. Ready for AJAX call to server

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -3,10 +3,22 @@ var Home = function() {
 
 	var logIn;
 	var signUp;
-
+	var departmentList;
+	var templateDepartment;
+	
 	var defaultColor = "#00b85c";
 	var selectedColor = "#00a653"
 
+	var makeGetRequest = function(url, onSuccess, onFailure) {
+       $.ajax({
+           type: 'GET',
+           url: apiUrl + url,
+           dataType: "json",
+           success: onSuccess,
+           error: onFailure
+       });
+   };
+	
 	var attachLogInHandler = function() {
 		logIn.on('click', function() {
 			signUp.css('background-color', defaultColor);
@@ -48,14 +60,47 @@ var Home = function() {
 			show.slideDown();
 		});
 	};
-
+	var insertDepartment = function(dept){
+		var newElem = $(templateDepartment);
+		newElem.attr('value',dept);
+		newElem.html(dept);
+		newElem.removeAttr('selected');
+		newElem.removeAttr('disabled');
+		newElem.removeClass('default-option');
+		departmentList.append(newElem);
+		
+	};
+	var insertDepartmentList = function() {
+		var onSuccess = function(data){
+			//Take the returned list of departments and insert each one.
+			/* var len data.departments.length()
+			for(i=0; i < len; i++){
+				insertDepartment(data.departments[i]);
+			}
+			*/
+		};
+		var onFailure = function(){
+			/*print error message
+			console.error('could not get department list');
+			*/
+		};
+		//makeGetRequest(url_to_get_departments (main page), onSuccess, onFailure);
+		insertDepartment('Computer Science');
+		insertDepartment('Astronomy');
+		insertDepartment('History');
+	};
+	
 	var start = function() {
 		logIn = $('.log-in');
 		signUp = $('.sign-up');
-
+		departmentList = $('.department-input');
+		templateDepartment = $('.department-input .department-option')[0].outerHTML;
+		//departmentList.remove(templateDepartment);
+		//departmentList.html('');
 		attachLogInHandler();
 		attachSignUpHandler();
 		attachAdvancedSearchHandler();
+		insertDepartmentList();
 	};
 
 	return {
