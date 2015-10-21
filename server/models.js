3ed45e5b70bf02820825ae260b98d3021f637aa3
AlexKhodaverdian <AlexKhodaverdian@berkeley.edu>
2015-10-21 09:51:20 -0700

    Added Courses Table besides Foreign Keys

--- server/models.js
+++ server/models.js
@@ -5,6 +5,32 @@ var sequelize = new Sequelize(con_string);
 sequelize.sync()
 
 var Departments = sequelize.define('Departments', {
-  department: { type: Sequelize.STRING, primaryKey: true}
+  department_name: { type: Sequelize.STRING, primaryKey: true}
 })
+
+var Professors = sequelize.define('Professors', {
+  professor_name: { type: Sequelize.STRING, primaryKey: true}
+})
+
+
+var Courses = sequelize.define('Courses', {
+  name: { type: Sequelize.STRING, primaryKey: true},
+  number: { type: Sequelize.INTEGER, primaryKey: true},
+
+  type:  Sequelize.STRING,
+  title: Sequelize.STRING,
+  ccn: Sequelize.INTEGER,
+  units: Sequelize.DOUBLE,
+  time: Sequelize.STRING,
+  location: Sequelize.STRING,
+  final_slot: Sequelize.INTEGER,
+  limit: Sequelize.INTEGER,
+  enrolled: Sequelize.INTEGER,
+  waitlist: Sequelize.INTEGER,
+  note: Sequelize.STRING
+
+})
+
+Courses.sync()
 Departments.sync()
+Professors.sync()
