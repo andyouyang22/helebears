6a494bc6219e4fe03729c0616f00ace11840c631
AlexKhodaverdian <AlexKhodaverdian@berkeley.edu>
2015-10-21 22:01:09 -0700

    [BACKEND] Models fixed but need moving in their respective files

--- server/models.js
+++ server/models.js
@@ -21,6 +21,7 @@ var Courses = sequelize.define('Courses', {
   name_and_number: {type:Sequelize.STRING, primaryKey: true},
   professor_name: {
     type: Sequelize.STRING,
+    primaryKey: true,
     references: {
       model: Professors,
       key: 'professor_name',
@@ -28,6 +29,7 @@ var Courses = sequelize.define('Courses', {
   },
   department_name: {
     type: Sequelize.STRING,
+    primaryKey: true,
     references: {
       model: Departments,
       key: 'department_name',
@@ -64,25 +66,16 @@ var Reviews = sequelize.define("Reviews", {
    }
   },
 })
-// var Schedules = sequelize.define("Schedules", {
-//   unique_id: {type: Sequelize.STRING, primaryKey: true},
-//   name: {
-//     type: Sequelize.STRING,
-//     references: {
-//       model: Courses,
-//       key: 'name',
-//       deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-//    }
-//   },
-//   number: {
-//     type: Sequelize.INTEGER,
-//     references: {
-//       model: Courses,
-//       key: 'number',
-//       deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-//    }
-//   }
-// })
+var Schedules = sequelize.define("Schedules", {
+  unique_id: {type: Sequelize.STRING, primaryKey: true},
+  name_and_number: {
+    type: Sequelize.STRING,
+    references: {
+      model: Courses,
+      key: 'name_and_number',
+   }
+  }
+})
 var Sections = sequelize.define("Sections", {
   discussion: {type: Sequelize.STRING, primaryKey: true},
   type: {type: Sequelize.STRING, primaryKey: true},
@@ -93,7 +86,7 @@ var Sections = sequelize.define("Sections", {
   limit: Sequelize.INTEGER,
   enrolled: Sequelize.INTEGER,
   waitlist: Sequelize.INTEGER,
-    name_and_number: {
+  name_and_number: {
     type: Sequelize.STRING,
     references: {
       model: Courses,
@@ -107,23 +100,5 @@ Departments.sync()
 Professors.sync()
 Reviews.sync()
 Users.sync()
-//Schedules.drop()
-//Schedules.sync()
-//Sections.drop()
-//Sections.drop()
-//Sections.sync()
-//var p = Professors.build({professor_name: "Kozai"})
-//p.save()
-
-//var d = Departments.build({department_name: "Math Science"})
-//d.save()
-//var c = Courses.build({name: "62C", number:11, name_and_number:"62C 11",professor_name:"Kozai", department_name:"Math Science", type: "LEC", title: "Datastructure", ccn: 5, units: 2.5, time: "MW", location: "WHEELER", final_slot: 5, limit: 10, enrolled: 5, waitlist: 0, note: ""})
-//c.save()
-//var r = Reviews.build({rating_1:5, rating_2:3, rating_3:5, review:"Terrible", professor_name: "Denero"})
-//r.save()
-//var c = Sections.build({discussion: "11.185", type: "5", name_and_number:"62C 11", instructor: "END", ccn: 5, time:"MW", location: "as", limit: 10, enrolled: 5, waitlist: 10})
-//c.save()
-// var s = Schedules.build({discussion: "99.123", name:"1233", number: 001})
-// s.save()
-
-Sections.findAll().then(function(courses){console.log(courses)})
+Schedules.sync()
+Sections.sync()
---
+++ server/models/models_test_temp.js
@@ -0,0 +1,16 @@
+
+//var p = Professors.build({professor_name: "Kozai"})
+//p.save()
+
+//var d = Departments.build({department_name: "Math Science"})
+//d.save()
+//var c = Courses.build({name: "62C", number:11, name_and_number:"62C 11",professor_name:"Kozai", department_name:"Math Science", type: "LEC", title: "Datastructure", ccn: 5, units: 2.5, time: "MW", location: "WHEELER", final_slot: 5, limit: 10, enrolled: 5, waitlist: 0, note: ""})
+//c.save()
+//var r = Reviews.build({rating_1:5, rating_2:3, rating_3:5, review:"Terrible", professor_name: "Denero"})
+//r.save()
+//var c = Sections.build({discussion: "11.185", type: "5", name_and_number:"62C 11", instructor: "END", ccn: 5, time:"MW", location: "as", limit: 10, enrolled: 5, waitlist: 10})
+//c.save()
+//var s = Schedules.build({unique_id: "99.12333", name_and_number:"62C 11"})
+//s.save()
+
+//Schedules.findAll().then(function(courses){console.log(courses)})
