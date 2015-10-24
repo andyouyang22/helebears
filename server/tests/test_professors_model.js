99e97c8034e19e2731bd9b12eb4ab3a36a6fbec0
AlexKhodaverdian <AlexKhodaverdian@berkeley.edu>
2015-10-23 17:19:25 -0700

    [BACKEND] Two tests written for Professors

--- server/tests/test_professors_model.js
+++ server/tests/test_professors_model.js
@@ -3,14 +3,28 @@ var professors_model = require("../models/professors_model")
 
 var Professors = professors_model.Professors
 
-describe('Professors', function() {
-  describe('findAll()', function () {
-  	it('Queried Professors Model Successfully with findAll()', function () {
-  	Professors.findAll().then(function(professors){
+
+describe('============== Professors ==============', function() {
+	it('Queried Professors Model Successfully with findAll()', function (done) {
+  		Professors.findAll().then(function(professors){
   			assert(true)
+
+  			done()
+
     	}).error(function(err){
     		assert(false)
+    		done()
     	})
-  })
-  });
+  	})
+  	it('Queried Professors Model Successfully with findOne()', function (done) {
+  		Professors.findOne().then(function(professor){
+  			assert(true)
+
+  			done()
+
+    	}).error(function(err){
+    		assert(false)
+    		done()
+    	})
+  	})
 });
