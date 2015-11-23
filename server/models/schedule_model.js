a109aa9dd8d1aab24747bce9d68d993bbb1db7f3
Andy Ouyang <andy@Andys-MacBook-Pro-2.local>
2015-11-23 14:11:51 -0800

    Back End: Minor non-semantic clarity changes

--- server/models/schedule_model.js
+++ server/models/schedule_model.js
@@ -9,64 +9,61 @@ var Sequelize = sequelize_modules.Sequelize
 var Courses = course_models.Courses;
 
 var Schedules = sequelize.define("Schedules", {
-  unique_id: {type: Sequelize.STRING, primaryKey: true},
-  name_and_number: {
-      type: Sequelize.STRING,
-      primaryKey: true,
-  },
-  course_time: Sequelize.STRING,
-  section_time: Sequelize.STRING,
-  lab_time: Sequelize.STRING
+	unique_id: {type: Sequelize.STRING, primaryKey: true},
+	name_and_number: {
+		type : Sequelize.STRING,
+		primaryKey : true,
+	},
+	course_time: Sequelize.STRING,
+	section_time: Sequelize.STRING,
+	lab_time: Sequelize.STRING
 })
 Schedules.sync()
 
 var scheduleModel = {
-    preprocess: function(userDataValues,type,res) {
-        if (type === 'get'){
-            scheduleModel.searchQuery(userDataValues,res);
-        }
-
-
-    },
-    searchQuery: function(unique_id,res) {
-        Schedules.findAll({where: {"unique_id": unique_id}}).then(
-            function(results){
-                stripped_results  = []
-                for(i = 0; i < results.length;i++){
-                    stripped_results.push(results[i].dataValues)
-                }
-                res.json({status:1, "results": stripped_results})
-            }).catch(function(err) {
-                res.json({status:-1, errors:["Unable to correctly retrieve all departments",err]})
-            })
-    },
-    createQuery: function (data, res) {
-        Schedules.create(data).then(function(results){
-            res.json({
-                status:1
-            })
-        }).catch(function(err){
-            res.json({status: -1, errors:['Course already added for user',err]});
-        });
-    },
-    removeQuery: function (data, res) {
-        Schedules.findAll({where: data}).then(function(results){
-            results[0].destroy().then(function(){
-                res.json({status: 1})
-            }).catch(function(err){
-                res.json({status: -1, errors:['Error destroying course, course exists',err]});
-            })
-        }).catch(function(err){
-            res.json({status: -1, errors:['Error removing course, course may not exist',err]});
-        });
-    },
-
-    controller: function(userDataValues,type,res) {
-        // The controller is responsible to navigate between preprocess, process and postprocess and provide
-        // the answer to the client the required format.
-        scheduleModel.preprocess(userDataValues,type,res)
-    }
-
+	preprocess: function(userDataValues,type,res) {
+		if (type === 'get'){
+			scheduleModel.searchQuery(userDataValues,res);
+		}
+	},
+	searchQuery: function(unique_id,res) {
+		Schedules.findAll({where: {"unique_id": unique_id}}).then(
+			function(results){
+				stripped_results  = []
+				for(i = 0; i < results.length;i++){
+					stripped_results.push(results[i].dataValues)
+				}
+				res.json({status:1, "results": stripped_results})
+			}).catch(function(err) {
+				res.json({status:-1, errors:["Unable to correctly retrieve all departments",err]})
+			})
+	},
+	createQuery: function (data, res) {
+		Schedules.create(data).then(function(results){
+			res.json({
+				status:1
+			})
+		}).catch(function(err){
+			res.json({status: -1, errors:['Course already added for user',err]});
+		});
+	},
+	removeQuery: function (data, res) {
+		Schedules.findAll({where: data}).then(function(results){
+			results[0].destroy().then(function(){
+				res.json({status: 1})
+			}).catch(function(err){
+				res.json({status: -1, errors:['Error destroying course, course exists',err]});
+			})
+		}).catch(function(err){
+			res.json({status: -1, errors:['Error removing course, course may not exist',err]});
+		});
+	},
+	controller: function(userDataValues,type,res) {
+		// The controller is responsible to navigate between preprocess, process
+		// and postprocess and provide the answer to the client the required format.
+		scheduleModel.preprocess(userDataValues,type,res)
+	}
 };
+
 module.exports.Schedules = Schedules;
 module.exports.scheduleModel = scheduleModel;
--- server/routes/schedules.js
+++ server/routes/schedules.js
@@ -3,43 +3,58 @@ var router = express.Router();
 
 var schedules_model = require("../models/schedule_model")
 var scheduleModel = schedules_model.scheduleModel
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    if("user" in req) {
-        scheduleModel.controller(req.user.dataValues.email, 'get', res)
-    }
-    else{
-        res.json({status:-1, errors: ["No user object sent"]})
-    }
+	if ("user" in req) {
+		scheduleModel.controller(req.user.dataValues.email, 'get', res)
+	}
+	else {
+		res.json({
+			status : -1,
+			errors : ["No user object sent"]
+		});
+	}
 });
 
 /* POST request for adding a class to schedule */
 router.post('/add',function(req, res, next) {
-    if(!("user" in req)) {
-        res.json({status:-1, errors: ["No user object sent"]})
-    }
-    if(!("name_and_number" in req.body)) {
-        res.json({status:-1, errors: ["name_and_number not sent with post request's body"]})
-    }
-    else{
-        req.body.unique_id = req.user.dataValues.email
-        scheduleModel.createQuery(req.body, res)
-    }
+	if (!("user" in req)) {
+		res.json({
+			status : -1,
+			errors : ["No user object sent"]
+		});
+	}
+	if (!("name_and_number" in req.body)) {
+		res.json({
+			status : -1,
+			errors : ["name_and_number not sent with post request's body"]
+		});
+	}
+	else {
+		req.body.unique_id = req.user.dataValues.email
+		scheduleModel.createQuery(req.body, res)
+	}
 });
 
 /* POST request for removing a class to schedule */
 router.post('/remove',function(req, res, next) {
-    if(!("user" in req)) {
-        res.json({status:-1, errors: ["No user object sent"]})
-    }
-    if(!("name_and_number" in req.body)) {
-        res.json({status:-1, errors: ["name_and_number not sent with post request's body"]})
-    }
-    else{
-        req.body.unique_id = req.user.dataValues.email
-        scheduleModel.removeQuery(req.body, res)
-    }
+	if (!("user" in req)) {
+		res.json({
+			status : -1,
+			errors : ["No user object sent"]
+		});
+	}
+	if (!("name_and_number" in req.body)) {
+		res.json({
+			status : -1,
+			errors : ["name_and_number not sent with post request's body"]
+		});
+	}
+	else {
+		req.body.unique_id = req.user.dataValues.email
+		scheduleModel.removeQuery(req.body, res)
+	}
 });
-
 
 module.exports = router;
