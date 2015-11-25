bd8d93d35e370e1f59fa5b0fbbda322bc7374d33
ryankashi <ryankashi@berkeley.edu>
2015-11-24 22:41:12 -0800

    Update ajax.js

--- static/scripts/util/ajax.js
+++ static/scripts/util/ajax.js
@@ -1,8 +1,8 @@
 var parse = require('./parse.js');
 var time  = require('./time.js');
 
-var apiUrl = 'http://protected-refuge-7067.herokuapp.com';
-//var apiUrl = '';
+//var apiUrl = 'http://protected-refuge-7067.herokuapp.com';
+var apiUrl = '';
 
 var queryify = function(query) {
 	query = JSON.stringify(query);
