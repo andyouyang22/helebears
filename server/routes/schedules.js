3b7e77bc2c898422ef065af27d4eb96de654d52a
Andy Ouyang <andy@Andys-MacBook-Pro-2.local>
2015-11-24 20:22:44 -0800

    Back End: Non-semeantic formatting changes to routes/schedule.js

--- server/routes/schedules.js
+++ server/routes/schedules.js
@@ -5,25 +5,35 @@ var schedules_model = require("../models/schedule_model")
 var scheduleModel = schedules_model.scheduleModel
 var course_models = require("../models/course_models")
 var courseModel = course_models.courseModel;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    if("user" in req) {
-        scheduleModel.controller(req.user.dataValues.email, 'get', res)
+    if ("user" in req) {
+        scheduleModel.controller(req.user.dataValues.email, 'get', res);
     }
-    else{
-        res.json({status:-1, errors: ["No user object sent"]})
+    else {
+        res.json({
+            status : -1,
+            errors : ["No user object sent"],
+        });
     }
 });
 
 /* POST request for adding a class to schedule */
 router.post('/add',function(req, res, next) {
-    if(!("user" in req)) {
-        res.json({status:-1, errors: ["No user object sent"]})
+    if (!("user" in req)) {
+        res.json({
+            status : -1,
+            errors : ["No user object sent"],
+        });
     }
-    if(!("name_and_number" in req.body)) {
-        res.json({status:-1, errors: ["name_and_number not sent with post request's body"]})
+    if (!("name_and_number" in req.body)) {
+        res.json({
+            status : -1,
+            errors : ["name_and_number not sent with post request's body"],
+        });
     }
-    else{
+    else {
         req.body.unique_id = req.user.dataValues.email;
         courseModel.aggregateRecommendations(req.body, res)
     }
@@ -31,17 +41,22 @@ router.post('/add',function(req, res, next) {
 
 /* POST request for removing a class to schedule */
 router.post('/remove',function(req, res, next) {
-    if(!("user" in req)) {
-        res.json({status:-1, errors: ["No user object sent"]})
+    if (!("user" in req)) {
+        res.json({
+            status : -1,
+            errors : ["No user object sent"],
+        });
     }
-    if(!("name_and_number" in req.body)) {
-        res.json({status:-1, errors: ["name_and_number not sent with post request's body"]})
+    if (!("name_and_number" in req.body)) {
+        res.json({
+            status : -1,
+            errors : ["name_and_number not sent with post request's body"],
+        });
     }
-    else{
+    else {
         req.body.unique_id = req.user.dataValues.email
         scheduleModel.removeQuery(req.body, res)
     }
 });
-
 
 module.exports = router;
