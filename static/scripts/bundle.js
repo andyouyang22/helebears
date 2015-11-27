521aeebc8a23c70b332439def5e5ab6b533c4cc6
Andy Ouyang <andy@Andys-MBP-2.san.rr.com>
2015-11-26 18:35:04 -0800

    Front End: Removed randomly-generated CCNs (from ITER2)

--- static/scripts/bundle.js
+++ static/scripts/bundle.js
@@ -24395,34 +24395,12 @@ module.exports = {
 
 var time = require('./time.js');
 
-var generateCCN = function (ccn) {
-	ccn = ccn + "";
-	for (var i = ccn.length; i < 5; i++) {
-		ccn = "0" + ccn;
-	}
-	return ccn;
-};
-
 module.exports = {
-	schedule: function (data) {
-		if (data.status == -1) {
-			console.log("Failed to load user's schedule");
-			console.log("Errors: " + data.errors);
-			return;
-		}
+	courses: function (data) {
 		var courses = [];
-		data.results.forEach(function (result) {
-			var course = {
-				name: result.name_and_number,
-				time: result.course_time,
-				room: result.location,
-				ccn: result.ccn
-			};
-			if (course.ccn == undefined) {
-				course.ccn = that.ccn(course.name, course.time);
-			}
-			courses.push(course);
-		});
+		for (var i = 0; i < data.results.length; i++) {
+			courses.push(data.results[i].name);
+		}
 		return courses;
 	},
 	departments: function (data) {
@@ -24431,13 +24409,6 @@ module.exports = {
 			depts.push(data.results[i].department_name);
 		}
 		return depts;
-	},
-	courses: function (data) {
-		var courses = [];
-		for (var i = 0; i < data.results.length; i++) {
-			courses.push(data.results[i].name);
-		}
-		return courses;
 	},
 	results: function (data) {
 		var results = [];
@@ -24460,12 +24431,33 @@ module.exports = {
 			lec.sections.forEach(function (sec) {
 				course.sections.push({
 					time: time.convert(sec.time),
-					ccn: generateCCN(sec.ccn)
+					ccn: sec.ccn
 				});
 			});
 			results.push(course);
 		});
 		return results;
+	},
+	schedule: function (data) {
+		if (data.status == -1) {
+			console.log("Failed to load user's schedule");
+			console.log("Errors: " + data.errors);
+			return;
+		}
+		var courses = [];
+		data.results.forEach(function (result) {
+			var course = {
+				name: result.name_and_number,
+				time: result.course_time,
+				room: result.location,
+				ccn: result.ccn
+			};
+			if (course.ccn == undefined) {
+				course.ccn = that.ccn(course.name, course.time);
+			}
+			courses.push(course);
+		});
+		return courses;
 	},
 	split: function (course) {
 		var split = [];
