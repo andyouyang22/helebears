939290a4a542c7fbd323932d8bb7b488cf5b5f0e
Nir Shern <nirshtern@berkeley.edu>
2015-12-06 01:00:42 -0800

    [AUTHENTICATION] updated google callback url to refelct heroku

--- server/config/auth.js
+++ server/config/auth.js
@@ -7,6 +7,6 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '753668621131-36g6ge4gcnlaldr3easvgu83nsb6n88r.apps.googleusercontent.com',
         'clientSecret'  : '8fsUiVGAyHua6nmDtd9O21sj',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'   : 'http://protected-refuge-7067.herokuapp.com/auth/google/callback'
     }
 };
